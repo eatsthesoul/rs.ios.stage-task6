111a613cad80dd3535768d91830be56590550d9c
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-06-19 16:08:39 +0300

    feat: add solution for exercise-1

--- Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
@@ -1,18 +1,48 @@
 import Foundation
 
 class CoronaClass {
- 
-     var seats = [Int]()
-
-     init(n: Int) {
- 
-     }
-     
-     func seat() -> Int {
-        -1
-     }
-     
-     func leave(_ p: Int) {
-         
-     }
+    
+    var seats = [Int]()
+    let n: Int
+    
+    init(n: Int) {
+        self.n = n
+    }
+    
+    func seat() -> Int {
+        
+        if !seats.contains(0) && n > 0 {
+            seats.insert(0, at: 0)
+            return 0
+        }
+        
+        if !seats.contains(n - 1) {
+            seats.append(n - 1)
+            return n - 1
+        }
+        
+        var distanceArray = [Int]()
+        
+        for i in 0..<seats.count-1 {
+            let difference = seats[i + 1] - seats[i] - 1
+            
+            if difference % 2 == 1 {
+                distanceArray.append(difference / 2 + 1)
+            } else {
+                distanceArray.append(difference / 2)
+            }
+        }
+        
+        let maxDistance = distanceArray.max()!
+        let indexOfDistance = distanceArray.firstIndex(of: maxDistance)!
+        
+        let seat = seats[indexOfDistance] + maxDistance
+        seats.insert(seat, at: indexOfDistance + 1)
+        return seat
+    }
+    
+    func leave(_ p: Int) {
+        guard let index = seats.firstIndex(of: p) else { return }
+        seats.remove(at: index)
+    }
 }
